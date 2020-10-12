96dece75e427b288cfd2ffb4004617068feec869
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2020-10-12 17:52:35 -0500

    Updates

---
+++ precache-manifest.783f9a6b992562e5a956b7b7ada75abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8005bac6db7a12bc47d70d70689271e",
+    "url": "/react-gallery-app-V2/index.html"
+  },
+  {
+    "revision": "9dbbc65006942a55be77",
+    "url": "/react-gallery-app-V2/static/css/main.4dd4c76a.chunk.css"
+  },
+  {
+    "revision": "cae92e5477fe77c8fa94",
+    "url": "/react-gallery-app-V2/static/js/2.61927713.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gallery-app-V2/static/js/2.61927713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbbc65006942a55be77",
+    "url": "/react-gallery-app-V2/static/js/main.d052d0f4.chunk.js"
+  },
+  {
+    "revision": "54cd1e09fb90847d362e",
+    "url": "/react-gallery-app-V2/static/js/runtime-main.29f18c1b.js"
+  }
+]);
